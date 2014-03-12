8d9b6872aaf1d6ef2bdedff6df041374227c6ab6
Brid Mackey <brid.mackey@gmail.com>
2014-03-12 22:06:31 +0000

    header and footer in place

--- client/default/app/views/Information.js
+++ client/default/app/views/Information.js
@@ -1,5 +1,7 @@
 app.views.Information = Ext.extend(Ext.Panel, {
   title: 'Information',
+  iconCls: 'info',
+  style: 'background-color:#6d6e71;',
   scroll: 'vertical',
   height: '100%',
   layout: 'fit',
@@ -13,7 +15,7 @@ app.views.Information = Ext.extend(Ext.Panel, {
   	{
   		dock: 'top',
   		xtype: 'toolbar',
-      	title: 'Information',
+      title: 'Information',
   		items: [
   			{
   				html: '<img src="app/images/back.png"/>',
@@ -23,13 +25,50 @@ app.views.Information = Ext.extend(Ext.Panel, {
             		app.stores.stocks.removeAll();
   				}
   			},
+  			{
+  				xtype: 'spacer'
+  			},
+  			{
+  				html: '<img class="headerLogo" src="app/images/homeLogo.png"/>'
+  			},
   		]
-  	}
+  	},
+
+    {
+      dock: 'bottom',
+      xtype: 'panel',
+      height: 30,
+      align: 'center',
+      pack: 'center',
+      style: 'text-align:center',
+      cls: 'btm-nav',
+      items: [
+
+        {
+          xtype: 'panel',
+          height:10,
+          style: 'text-align:center',
+          html: '<img class="circle" src="app/images/circle.png" />'
+        },
+        
+        {
+          html: '<img class="menuIcon" src="app/images/icons/menu.png"/>',
+          //     hidden: app.hideBack || false,
+          // handler: function() {
+          //   app.views.viewport.setActiveItem(app.views.home, {type: 'slide', direction: 'right'});
+          //       app.stores.stocks.removeAll();
+          // }
+        },
+      ]
+    },
+
   ],
   
   items: [
     {
+    	floating: true,
       html: 'Information Goes Here'
-    }
-  ]
+    },
+  ],
+
 });
--- client/default/app/views/Project.js
+++ client/default/app/views/Project.js
@@ -1,6 +1,31 @@
 app.views.Project = Ext.extend(Ext.Panel, {
   title: 'Project',
   scroll: 'vertical',
+  height: '100%',
+  layout: 'fit',
+
+  listeners: {
+    beforeshow: function() {
+    },
+  },
+
+  dockedItems: [
+  	{
+  		dock: 'top',
+  		xtype: 'toolbar',
+      	title: 'Information',
+  		items: [
+  			{
+  				html: '<img src="app/images/back.png"/>',
+          		hidden: app.hideBack || false,
+  				handler: function() {
+  					app.views.viewport.setActiveItem(app.views.home, {type: 'slide', direction: 'right'});
+            		app.stores.stocks.removeAll();
+  				}
+  			},
+  		]
+  	}
+  ],
   
   items: [
     {
